a27cceee554fd8e892f7f60f6b134aa44d2d4e27
José Ramón Rivera <joseramonrivera21@gmail.com>
2024-11-05 22:24:24 -0600

    Fixes Tests for vthe vault project. Adds missing Anchor.toml fore NFT staking.

--- vault/programs/anchor-vault/src/lib.rs
+++ vault/programs/anchor-vault/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::{prelude::*, system_program::{Transfer, transfer}};
 
-declare_id!("47EArnb8zxsre5mXfboPR47dFqgVAcZ6P5tqB2cdiS7G");
+declare_id!("GngcpYJB9cGCs2ffwNSJyiGHvyRpAmf7dk978JGPm57D");
 
 #[program]
 pub mod anchor_vault {
@@ -45,15 +45,6 @@ pub struct Initialize<'info> {
     )]
     pub vault: SystemAccount<'info>,
     pub system_program: Program<'info, System>,
-}
-
-impl<'info> Initialize<'info> {
-    pub fn initialize(&mut self, bumps: &InitializeBumps) -> Result<()> {
-        self.vault_state.vault_bump = bumps.vault;
-        self.vault_state.state_bump = bumps.vault_state;
-
-        Ok(())
-    }
 }
 
 #[derive(Accounts)]
@@ -112,6 +103,25 @@ pub struct Close<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[account]
+pub struct VaultState {
+    pub vault_bump: u8,
+    pub state_bump: u8,
+}
+
+impl Space for VaultState {
+    const INIT_SPACE: usize = 8 + 1 + 1;
+}
+
+impl<'info> Initialize<'info> {
+    pub fn initialize(&mut self, bumps: &InitializeBumps) -> Result<()> {
+        self.vault_state.vault_bump = bumps.vault;
+        self.vault_state.state_bump = bumps.vault_state;
+
+        Ok(())
+    }
+}
+
 impl<'info> Deposit<'info> {
     pub fn deposit(&mut self, amount: u64) -> Result<()> {
         let cpi_program = self.system_program.to_account_info();
@@ -124,7 +134,7 @@ impl<'info> Deposit<'info> {
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
 
         transfer(cpi_ctx, amount)?;
-        
+
         Ok(())
     }
 }
@@ -134,11 +144,15 @@ impl<'info> Withdraw<'info> {
         let cpi_program = self.system_program.to_account_info();
 
         let cpi_accounts = Transfer {
-            from: self.user.to_account_info(),
-            to: self.vault.to_account_info(),
+            from: self.vault.to_account_info(),
+            to: self.user.to_account_info(),
         };
 
-        let seeds = &[b"vault", self.vault.to_account_info().key.as_ref(),&[self.vault_state.vault_bump]];
+        let seeds = &[
+            b"vault",
+            self.vault_state.to_account_info().key.as_ref(),
+            &[self.vault_state.vault_bump]];
+
         let signer_seeds = &[&seeds[..]];
         let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
 
@@ -147,7 +161,6 @@ impl<'info> Withdraw<'info> {
         Ok(())
     }
 }
-
 impl<'info> Close<'info> {
     pub fn close(&mut self) -> Result<()> {
         let cpi_program = self.system_program.to_account_info();
@@ -157,10 +170,13 @@ impl<'info> Close<'info> {
             to: self.user.to_account_info(),
         };
 
-        let seeds = &[b"vault",
+        let seeds = &[
+            b"vault",
             self.vault_state.to_account_info().key.as_ref(),
             &[self.vault_state.vault_bump]];
+
         let signer_seeds = &[&seeds[..]];
+
         let cpi_ctx = CpiContext::new_with_signer(cpi_program,
                                                   cpi_accounts,
                                                   signer_seeds);
@@ -169,13 +185,4 @@ impl<'info> Close<'info> {
 
         Ok(())
     }
-}
-#[account]
-pub struct VaultState {
-    pub vault_bump: u8,
-    pub state_bump: u8,
-}
-
-impl Space for VaultState {
-    const INIT_SPACE: usize = 8 + 1 + 1;
 }
